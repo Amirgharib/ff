0d2a47bf7acfe027e8f9175f69696e690cc3e83c
Amir Ghareeb <91801875+Amirgharib@users.noreply.github.com>
2023-06-07 18:18:46 +0300

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,41 @@
+document.querySelector(".body1 .re").onclick= function(){
+document.querySelector("body").style="background-color :red "
+document.querySelector(".body0 .right .white").style="z-index : 0 "  
+document.querySelector(".body0 .right .gray").style="z-index : 0 "
+document.querySelector(".body0 .right .blue").style="z-index : 0 "
+document.querySelector(".body0 .right .gold").style="z-index : 0 " 
+document.querySelector(".body0 .right .red").style="z-index : 1 " 
+}
+
+document.querySelector(".body1 .gol").onclick= function(){
+    document.querySelector("body").style="background-color :goldenrod " 
+    document.querySelector(".body0 .right .white").style="z-index : 0 "  
+document.querySelector(".body0 .right .gray").style="z-index : 0 "
+document.querySelector(".body0 .right .blue").style="z-index : 0 "
+document.querySelector(".body0 .right .red").style="z-index : 0 " 
+    document.querySelector(".body0 .right .gold").style="z-index : 1 " 
+    }
+    document.querySelector(".body1 .whit").onclick= function(){
+        document.querySelector("body").style="background-color :#a9a1a1;  " 
+        document.querySelector(".body0 .right .gold").style="z-index : 0 "  
+    document.querySelector(".body0 .right .gray").style="z-index : 0 "
+    document.querySelector(".body0 .right .blue").style="z-index : 0 "
+    document.querySelector(".body0 .right .red").style="z-index : 0 " 
+        document.querySelector(".body0 .right .white").style="z-index : 1 " 
+        }
+        document.querySelector(".body1 .gra").onclick= function(){
+            document.querySelector("body").style="background-color :#312e2e " 
+            document.querySelector(".body0 .right .white").style="z-index : 0 "  
+        document.querySelector(".body0 .right .gold").style="z-index : 0 "
+        document.querySelector(".body0 .right .blue").style="z-index : 0 "
+        document.querySelector(".body0 .right .red").style="z-index : 0 " 
+            document.querySelector(".body0 .right .gray").style="z-index : 1 " 
+            }
+            document.querySelector(".body1 .blu").onclick= function(){
+                document.querySelector("body").style="background-color :#6c9fa4 " 
+                document.querySelector(".body0 .right .white").style="z-index : 0 "  
+            document.querySelector(".body0 .right .gray").style="z-index : 0 "
+            document.querySelector(".body0 .right .gold").style="z-index : 0 "
+            document.querySelector(".body0 .right .red").style="z-index : 0 " 
+                document.querySelector(".body0 .right .blue").style="z-index : 1 " 
+                }
